a05688cd9092915e866ee9a4a1e931fc6c061761
Abduraxmonov <126749705+Avazbek-Abduraxmonov@users.noreply.github.com>
2023-09-28 19:56:09 +0500

    Python Practice
    Python bo'yicha amaliyot

---
+++ python_practice.py
@@ -0,0 +1,131 @@
+# x = [1, 2, 3]
+# y = list(reversed(x))
+
+# if y == x:
+#     print("Nap")
+# else:
+#     print("Nope")
+
+# print("End of the program")
+
+# def calculate_average():
+#     a = int(input("Enter number: "))
+#     b = int(input("Enter number: "))
+#     c = int(input("Enter number: "))
+
+#     result = (a + b + c) / 2
+
+#     return result
+
+# average = calculate_average()
+# print(average)
+# print("the end")
+
+# def function():
+# 	p = 3.14
+
+#  	l = 2 * p
+#  	return l
+
+# x = function()
+# print(x)
+
+
+# def function(num):
+# 	if num >= 10 and num <= 99 and num % 2 == 0:
+# 		return True
+
+# 	else:
+# 		return False
+
+
+
+# x = function(23)
+# print(x)
+
+
+
+# Primetri va Yuzini topuvchi funksiya
+
+
+# def function(a):
+# 	P = 4*a
+# 	S = a**a
+# 	result = P,S
+# 	return result
+
+# x =  function(3)
+# print(x)
+
+
+# def p_s(a):
+# 	V = a**3
+# 	S = 6*(a**a)
+
+# 	result = [V,S]
+# 	return result
+
+# x = p_s(2)
+
+# print(x)
+
+# def function(a,b,c):
+# 	"""
+# 	Paralapiped va Hajmi va tola sirti ni topadigan function
+# 	"""
+
+# 	V=a*b*c
+# 	S = 2*(a*b+b*c+a*c)
+
+# 	result = [V,S]
+
+# 	return result
+
+# math = function(2,2,2)
+# print(math)
+
+# def function(a,b):
+
+# 	math = (a+b)/2
+
+# 	return math
+
+# x = function(3,3)
+# print(x)
+
+# print(bool(''), bool(' ')) # False True
+
+
+# def function(a, b):
+#   the_end = "The End program"
+#   try:
+
+# 		if a > 0:
+# 		  p = a * b
+# 		  s = a * a
+# 		  result = [p, s]		  
+# 	except:
+# 		print("0 dan kichik son kiritdingiz")
+# return result
+
+# y = function(2, 2)
+# print(y)
+
+
+# =============== while tsikl ====================
+
+
+# names_dict = {}
+
+# while True:
+#     name = input("Enter your name: ")
+#     # names_dict[name] = names_dict.get(name, 0) + 1
+
+#     repeat = input("Do you want to enter another name? (yes/no): ")
+#     if repeat.lower() != "yes":
+#         break
+
+# print("Names entered")
+# print(names_dict)
+
+
